e7ee76caa8a3e5aec191cb05f5ac73b57a0032fb
rikzafsh <rikzafsh@gmail.com>
2025-06-03 10:47:40 +0700

    Add BookController and LoginController

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index() {
+
+        $title = '';
+        if(request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' dalam ' . $category->name;
+        }
+
+        if(request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' oleh ' . $author->name;
+        }
+
+        return view('book', [
+            "title" => "Book" . $title,
+            "posts" => Book::with(['author', 'category'])->latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
+        ]);
+    }
+
+    public function show(Book $book) {
+
+
+        return view('book', [
+            "title" => "Single Book", 
+            "keyt" => "Artikel",
+            "books" => $book
+        ]);
+
+    }
+}
--- database/migrations/2025_05_22_131216_create_books_table.php
+++ database/migrations/2025_05_22_131216_create_books_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->foreignId('author_id');
             $table->string('publisher');
             $table->integer('year');
